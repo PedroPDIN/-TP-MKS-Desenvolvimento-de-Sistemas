48fb11e78a80492803ff4e5765d1e8e7c80c7b83
Pedro Lima <pedro.oficial1969@gmail.com>
2024-05-23 09:38:09 -0300

    :white_check_mark: Teste ao componente footer.
    Adiciona instalacao no jest e testing-library
    (com ferramentas complementares), e teste unitario
     ao componente footer.

---
+++ __tests__/components/Footer.spec.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import Footer from "@/components/Footer";
+
+describe('Component Footer', () => {
+  it('footer elements rendered successfully.', () => {
+    render(<Footer />);
+
+    const elementFooter = screen.getByTestId("element-footer")
+
+    expect(elementFooter).toBeDefined();
+    expect(elementFooter.innerHTML).toEqual("MKS sistemas © Todos os direitos reservados");
+  })
+})
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ import style from "@/styles/components/_footer.module.scss";
 
 export default function Footer() {
   return (
-    <footer className={style.footer_container}>
+    <footer className={style.footer_container} data-testid="element-footer">
       MKS sistemas © Todos os direitos reservados
     </footer>
   );
